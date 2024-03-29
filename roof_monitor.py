057b80f2921510bbeca0fe97af3296f953d0aa70
Vilen Sharifov <eg013ra1n@users.noreply.github.com>
2024-03-29 21:58:41 +0200

    Update roof_monitor.py
    added extra exceptions for streaming

--- roof_monitor.py
+++ roof_monitor.py
@@ -12,26 +12,31 @@ def load_config():
 def roof_server_polling(server_address, server_port, status_file_path):
     while True:
         try:
-            # Establish a connection to the roof server
-            with socket.create_connection((server_address, server_port)) as sock:
+            with socket.create_connection((server_address, server_port), timeout=10) as sock:
+                sock.settimeout(10.0)
                 while True:
-                    # Read data from the socket
                     data = sock.recv(1024).decode('utf-8')
                     if not data:
-                        break  # Break the loop if no more data
+                        raise Exception("Connection closed by the server")
                     print(f"Data from Roof Server: {data}")
                     match = re.search(r'Stat=\s*(\d)', data)
                     if match:
                         stat = int(match.group(1))
-                        roof_status = 0 if stat == 0 else 1  # 0 if roof is open, 1 if roof is closed
+                        roof_status = 0 if stat == 0 else 1
                         update_status_file(status_file_path, roof_status)
                     else:
-                        update_status_file(status_file_path, 2)  # Unexpected data format
+                        update_status_file(status_file_path, 2)
+
+        except socket.timeout:
+            print("Connection timed out. Attempting to reconnect...")
+            update_status_file(status_file_path, 2)
+
         except Exception as e:
             print(f"Connection error: {e}")
-            update_status_file(status_file_path, 2)  # Connection error
-            print("Attempting to reconnect in 10 seconds...")
-            time.sleep(10)  # Wait for 10 seconds before retrying
+            update_status_file(status_file_path, 2)
+
+        print("Attempting to reconnect in 10 seconds...")
+        time.sleep(10)
 
 def update_status_file(status_file_path, status):
     try:
